3c6c17761e7b9469febe492f3973e923e451a798
Reanna Bakshani <110186374+reannab16@users.noreply.github.com>
2024-05-07 18:47:49 +0100

    Create env config file and connect to db file

---
+++ internal/config/config.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	
+
+	"github.com/joho/godotenv"
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+	Database struct {
+		MongoUri      string `envconfig:"MONGODB_URI" default:"mongodb://localhost:27017"`
+		AccessTimeout int    `envconfig:"MONGODB_ACCESS_TIMEOUT" default:"5"`
+	}
+}
+
+
+
+// It is initialized once when the application starts.
+var appConfig = &Config{}
+
+// This function provides access to the appConfig variable.
+func AppConfig() *Config {
+	return appConfig
+}
+
+// LoadConfig loads environment variables and populates appConfig.
+// It first attempts to load variables from a .env file and then
+// processes environment variables according to the Config struct tags.
+func LoadConfig() error {
+	godotenv.Load()
+	if err := envconfig.Process("", appConfig); err != nil {
+		return err
+	}
+
+	
+
+	return nil
+}
+
+
+
